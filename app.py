603b03f8c417e51c436771d9979ef3f027adb4c4
Grigoriy Kostryukov <kostryukov-grigoriy@mail.ru>
2021-12-20 20:38:12 +0300

    Added main menu

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 import tkinter as tk
+from tkinter import *
 from lib.models.user import User
 from lib.views.add_driver_form import DriverForm
-
 
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
+        self.eval('tk::PlaceWindow . center')
         self.user = User("postgres", "password")
-        self.btn = tk.Button(self, text="Открыть новое окно",
-                             command=self.open_driver_form)
-        self.btn.pack(padx=50, pady=20)
+        self.geometry("700x500")
+        menubar = tk.Menu(self)
+        self.config(menu=menubar)
+        drivers_menu = Menu(menubar, tearoff=0)
+        drivers_menu.add_command(label="Добавить", command=self.open_driver_form)
+        menubar.add_cascade(label="Водители", menu=drivers_menu)
 
     def open_driver_form(self):
         form = DriverForm(self)
@@ -20,3 +24,4 @@ class App(tk.Tk):
 if __name__ == "__main__":
     app = App()
     app.mainloop()
+
